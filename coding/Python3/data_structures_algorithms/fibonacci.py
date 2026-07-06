c360e9e126eebbe483acdbc6f9c74c6a3b3b92d7
David Kim <davidkim2552@gmail.com>
2026-07-06 18:34:06 -0400

    restructuring

---
+++ coding/Python3/data_structures_algorithms/fibonacci.py
@@ -0,0 +1,11 @@
+def fib_tab(n):
+    if n <= 1:
+        return n
+    dp = [0] * (n+1)
+    dp[1] = 1
+    for i in range(2,n+1):
+        dp[i] = dp[i-1]+dp[i-2]
+    return dp[n]
+
+for i in range(11):
+    print(f'{i}:{fib_tab(i)}')
